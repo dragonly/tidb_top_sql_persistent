032fa1666bf41636fb90b6f1117fc5d0295f57fb
dragonly <liyilongko@gmail.com>
2021-06-08 13:34:08 +0800

    basic proxy server

--- internal/app/monkey_server.go
+++ internal/app/monkey_server.go
@@ -17,8 +17,11 @@ limitations under the License.
 package app
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/pingcap/tipb/go-tipb"
 	"google.golang.org/grpc"
@@ -28,31 +31,79 @@ var _ tipb.TopSQLAgentServer = &monkeyServer{}
 
 type monkeyServer struct{}
 
-func (*monkeyServer) ReportPlanMeta(tipb.TopSQLAgent_ReportPlanMetaServer) error {
+func (*monkeyServer) ReportPlanMeta(stream tipb.TopSQLAgent_ReportPlanMetaServer) error {
+	log.Println("call ReportPlanMeta()")
+	resp := &tipb.EmptyResponse{}
+	stream.SendAndClose(resp)
 	return nil
 }
 
-func (*monkeyServer) ReportSQLMeta(tipb.TopSQLAgent_ReportSQLMetaServer) error {
+func (*monkeyServer) ReportSQLMeta(stream tipb.TopSQLAgent_ReportSQLMetaServer) error {
+	log.Println("call ReportSQLMeta()")
+	resp := &tipb.EmptyResponse{}
+	stream.SendAndClose(resp)
 	return nil
 }
 
 func (*monkeyServer) ReportCPUTimeRecords(stream tipb.TopSQLAgent_ReportCPUTimeRecordsServer) error {
+	log.Println("call ReportCPUTimeRecords()")
+	resp := &tipb.EmptyResponse{}
+	stream.SendAndClose(resp)
 	return nil
 }
 
+func startProxyServer(lisProxy net.Listener, toPort int) error {
+	toAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("localhost:%d", toPort))
+	if err != nil {
+		return err
+	}
+	log.Printf("top addr: %+v\n", toAddr)
+	for {
+		connFrom, err := lisProxy.Accept()
+		log.Printf("new client connection: %+v\n", connFrom)
+		if err != nil {
+			log.Printf("[proxy] failed to create connection from client, %v", err)
+			continue
+		}
+		go func() {
+			defer connFrom.Close()
+			connTo, err := net.DialTCP("tcp", nil, toAddr)
+			log.Printf("new upstream connection: %+v\n", connTo)
+			if err != nil {
+				log.Printf("[proxy] failed to create connection to upstream, %v", err)
+				return
+			}
+			defer connTo.Close()
+			wg := sync.WaitGroup{}
+			wg.Add(2)
+			go copy(&wg, connTo, connFrom)
+			go copy(&wg, connFrom, connTo)
+			wg.Wait()
+		}()
+	}
+}
+
+func copy(wg *sync.WaitGroup, dst io.Writer, src io.Reader) {
+	n, err := io.Copy(dst, src)
+	log.Printf("copied %d bytes from %v to %v, err: %v", n, src, dst, err)
+	wg.Done()
+}
+
 func StartMonkeyServer(proxyAddress string) {
+	// proxy
 	lisProxy, err := net.Listen("tcp", proxyAddress)
 	addrProxy := lisProxy.Addr().(*net.TCPAddr)
 	if err != nil {
-		log.Fatalf("[proxy] failed to listen on tcp address %s:%d, %v", addrProxy.IP, addrProxy.Port, err)
+		log.Fatalf("[proxy] failed to listen on TCP address %s:%d, %v", addrProxy.IP, addrProxy.Port, err)
 	}
 	defer lisProxy.Close()
 	log.Printf("[proxy] start listening on %s:%d", addrProxy.IP, addrProxy.Port)
 
+	// gRPC
 	lisGRPC, err := net.Listen("tcp", ":0")
 	addrGRPC := lisGRPC.Addr().(*net.TCPAddr)
 	if err != nil {
-		log.Fatalf("[gRPC] failed to listen on tcp address %s:%d, %v", addrGRPC.IP, addrGRPC.Port, err)
+		log.Fatalf("[gRPC] failed to listen on TCP address %s:%d, %v", addrGRPC.IP, addrGRPC.Port, err)
 	}
 	defer lisGRPC.Close()
 	log.Printf("[gRPC] start listening on %s:%d", addrGRPC.IP, addrGRPC.Port)
@@ -60,7 +111,12 @@ func StartMonkeyServer(proxyAddress string) {
 	server := grpc.NewServer()
 	tipb.RegisterTopSQLAgentServer(server, &monkeyServer{})
 	log.Printf("[gRPC] start gRPC server")
-	if err := server.Serve(lisGRPC); err != nil {
-		log.Fatalf("failed to start gRPC server: %v", err)
-	}
+	go func() {
+		if err := server.Serve(lisGRPC); err != nil {
+			log.Fatalf("failed to start gRPC server: %v", err)
+		}
+	}()
+
+	log.Println("[proxy] start proxying TCP traffic")
+	startProxyServer(lisProxy, addrGRPC.Port)
 }
