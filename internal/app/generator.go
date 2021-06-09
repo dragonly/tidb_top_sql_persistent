89dcd7c06fd00964f86b6414ee784e20a081bc54
dragonly <liyilongko@gmail.com>
2021-06-09 13:59:18 +0800

    write to influxdb

--- internal/app/generator.go
+++ internal/app/generator.go
@@ -19,29 +19,55 @@ package app
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
+	"sync"
 	"time"
 
 	influxdb "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/pingcap/tipb/go-tipb"
 )
 
 const (
-	sqlNum      = 200
-	instanceNum = 100
-	startTs     = 0
-	endTs       = 100
+	sqlNum         = 200
+	instanceNum    = 100
+	startTs        = 0
+	endTs          = 60 * 60 * 24
+	writeWorkerNum = 10
 )
 
 func GenerateCPUTimeRecords(recordChan chan *tipb.CPUTimeRecord) {
-	for ts := startTs; ts < endTs; ts++ {
+	count := 0
+	lastCount := 0
+	lastTime := time.Now()
+	qps := 0
+	for minute := startTs; minute < endTs/60; minute++ {
 		for i := 0; i < instanceNum; i++ {
 			for j := 0; j < sqlNum; j++ {
+				var tsList []uint64
+				var cpuTimeList []uint32
+				for i := 0; i < 60; i++ {
+					tsList = append(tsList, uint64(minute*60+i))
+					cpuTimeList = append(cpuTimeList, rand.Uint32()%1000)
+				}
 				recordChan <- &tipb.CPUTimeRecord{
 					SqlDigest:     []byte(fmt.Sprintf("i%d_sql%d", i, j)),
 					PlanDigest:    []byte(fmt.Sprintf("i%d_plan%d", i, j)),
-					TimestampList: []uint64{uint64(ts)},
-					CpuTimeMsList: []uint32{uint32(rand.Uint32() % 1000)},
+					TimestampList: tsList,
+					CpuTimeMsList: cpuTimeList,
+				}
+				count++
+				if count%100 == 0 {
+					log.Printf("generated %d records, qps %d\n", count, qps)
+					now := time.Now()
+					duration := now.Sub(lastTime)
+					if duration > time.Second {
+						qps = 60 * (count - lastCount) * int(time.Second) / int(duration)
+						lastCount = count
+						lastTime = now
+					}
 				}
 			}
 		}
@@ -82,10 +108,39 @@ func GeneratePlanMeta(planMetaChan chan *tipb.PlanMeta) {
 	}
 }
 
+func writeCPUTimeRecordsInfluxDB(wg *sync.WaitGroup, writeAPI api.WriteAPIBlocking, recordsChan chan *tipb.CPUTimeRecord) {
+	// i := 0
+	for {
+		records := <-recordsChan
+		var points []*write.Point
+		for i, ts := range records.TimestampList {
+			cpuTimeMs := records.CpuTimeMsList[i]
+			p := influxdb.NewPoint("cpu_time",
+				map[string]string{
+					"sql_digest":  string(records.SqlDigest),
+					"plan_digest": string(records.PlanDigest),
+				},
+				map[string]interface{}{
+					"cpu_time_ms": cpuTimeMs,
+				},
+				time.Unix(int64(ts), 0),
+			)
+			points = append(points, p)
+		}
+		writeAPI.WritePoint(context.TODO(), points...)
+		// i++
+		// if i%100 == 0 {
+		// 	log.Printf("write %d records\n", i)
+		// }
+	}
+}
+
 func WriteInfluxDB() {
-	recordChan := make(chan *tipb.CPUTimeRecord, 1)
-	sqlMetaChan := make(chan *tipb.SQLMeta, 1)
-	planMetaChan := make(chan *tipb.PlanMeta, 1)
+	recordChan := make(chan *tipb.CPUTimeRecord, writeWorkerNum)
+	sqlMetaChan := make(chan *tipb.SQLMeta, writeWorkerNum)
+	planMetaChan := make(chan *tipb.PlanMeta, writeWorkerNum)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go GenerateCPUTimeRecords(recordChan)
 	go GenerateSQLMeta(sqlMetaChan)
 	go GeneratePlanMeta(planMetaChan)
@@ -96,11 +151,17 @@ func WriteInfluxDB() {
 	url := "http://localhost:2333"
 	client := influxdb.NewClient(url, token)
 	writeAPI := client.WriteAPIBlocking(org, bucket)
-	p := influxdb.NewPoint("stat",
-		map[string]string{"uint": "temperatur"},
-		map[string]interface{}{"avg": 24.5, "max": 45},
-		time.Now(),
-	)
-	writeAPI.WritePoint(context.TODO(), p)
+	// t, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	// p := influxdb.NewPoint("stat",
+	// 	map[string]string{"uint": "temperatur"},
+	// 	map[string]interface{}{"avg": 24.5, "max": 46},
+	// 	t,
+	// )
+	// writeAPI.WritePoint(context.TODO(), p)
+	for i := 0; i < writeWorkerNum; i++ {
+		go writeCPUTimeRecordsInfluxDB(&wg, writeAPI, recordChan)
+	}
+	wg.Wait()
+
 	client.Close()
 }
